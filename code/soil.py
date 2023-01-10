c76ff9a0e0531efd0d11a627dac3daa6f558464c
Philip Steingrüber <philip.steingruber@gmail.com>
2023-01-10 10:55:06 +0100

    Soil tiling

--- code/soil.py
+++ code/soil.py
@@ -5,6 +5,7 @@ from pytmx.util_pygame import load_pygame
 
 from settings import LAYERS
 from settings import TILE_SIZE, DEBUG
+from support import import_folder_dict
 
 
 class SoilTile(pygame.sprite.Sprite):
@@ -23,13 +24,14 @@ class SoilLayer:
 
         # Graphics
         self.soil_surf = pygame.image.load('../graphics/soil/o.png')
+        self.soil_surfs = import_folder_dict('../graphics/soil')
 
         self.grid = self.create_soil_grid()
+        self.hit_rects = self.create_hit_rects()
+
         if DEBUG:
             for row in self.grid:
                 print(row)
-        self.hit_rects = self.create_hit_rects()
-        if DEBUG:
             print(self.hit_rects)
 
     @staticmethod
@@ -60,15 +62,59 @@ class SoilLayer:
                 y = rect.y // TILE_SIZE
                 if 'F' in self.grid[y][x]:
                     self.grid[y][x].append('X')
-                    # self.create_soil_tiles()
-                    SoilTile(pos=(rect.x, rect.y), surf=self.soil_surf, groups=[self.all_sprites, self.soil_sprites])
+                    self.create_soil_tiles()
 
-    # TODO Do we actually need this? Seems ineficcient to recreate all the soil tiles every time. Might be used for reset after sleep?
     def create_soil_tiles(self):
         self.soil_sprites.empty()
         for row_index, row in enumerate(self.grid):
             for col_index, cell in enumerate(row):
                 if 'X' in cell:
+                    top = 'X' in self.grid[row_index-1][col_index]
+                    bottom = 'X' in self.grid[row_index+1][col_index]
+                    left = 'X' in row[col_index-1]
+                    right = 'X' in row[col_index+1]
+
+                    tile_type = 'o'
+
+                    if all([top, bottom, left, right]):
+                        tile_type = 'x'
+
+                    # Horizontals
+                    if left and not any([top, right, bottom]):
+                        tile_type = 'r'
+                    if right and not any([top, left, bottom]):
+                        tile_type = 'l'
+                    if right and left and not any([top, bottom]):
+                        tile_type = 'lr'
+
+                    # Verticals
+                    if top and not any([right, left, bottom]):
+                        tile_type = 'b'
+                    if bottom and not any([right, left, top]):
+                        tile_type = 't'
+                    if bottom and top and not any([right, left]):
+                        tile_type = 'tb'
+
+                    # Corners
+                    if left and bottom and not any([top, right]):
+                        tile_type = 'tr'
+                    if right and bottom and not any([top, left]):
+                        tile_type = 'tl'
+                    if left and top and not any([bottom, right]):
+                        tile_type = 'br'
+                    if right and top and not any([bottom, left]):
+                        tile_type = 'bl'
+
+                    # T shapes
+                    if all([top, bottom, right]) and not left:
+                        tile_type = 'tbr'
+                    if all([top, bottom, left]) and not right:
+                        tile_type = 'tbl'
+                    if all([top, left, right]) and not bottom:
+                        tile_type = 'lrb'
+                    if all([left, bottom, right]) and not top:
+                        tile_type = 'lrt'
+
                     SoilTile(pos=(col_index*TILE_SIZE, row_index*TILE_SIZE),
-                             surf=self.soil_surf,
+                             surf=self.soil_surfs[tile_type],
                              groups=[self.all_sprites, self.soil_sprites])
--- code/support.py
+++ code/support.py
@@ -15,5 +15,15 @@ def import_images_from_folder(path: str) -> list[pygame.Surface]:
     return surface_list
 
 
+def import_folder_dict(path: str) -> dict[str: pygame.Surface]:
+    res = {}
+    for _, __, files in walk(path):
+        for filename in files:
+            name = filename.split('.')[0]
+            image = pygame.image.load(path + '/' + filename).convert_alpha()
+            res[name] = image
+    return res
+
+
 def increment_and_modulo(x: int, mod: int) -> int:
     return (x + 1) % mod
